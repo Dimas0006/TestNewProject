ec2203c56c3c4fb06005b7c3cb8349e389c6b150
Dima <97662429+Dimas0006@users.noreply.github.com>
2025-08-21 18:26:30 +0300

    test: add smoke test

---
+++ TestNewProjectUITests/SmokeTests.swift
@@ -0,0 +1,15 @@
+//
+//  SmokeTests.swift
+//  TestNewProjectUITests
+//
+//  Created by Дима Вакулюк on 2025-08-21.
+//
+
+import XCTest
+
+final class SmokeTests: XCTestCase {
+    func test_true_is_true() {
+        XCTAssertTrue(true)
+    }
+}
+
