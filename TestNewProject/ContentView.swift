76ed15eca9a869fd1bbb3255353bfbb7bb24a06d
Dima <97662429+Dimas0006@users.noreply.github.com>
2025-08-21 18:16:45 +0300

    Initial Commit

---
+++ TestNewProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestNewProject
+//
+//  Created by Дима Вакулюк on 2025-08-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestNewProject/TestNewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewProjectApp.swift
+//  TestNewProject
+//
+//  Created by Дима Вакулюк on 2025-08-21.
+//
+
+import SwiftUI
+
+@main
+struct TestNewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
